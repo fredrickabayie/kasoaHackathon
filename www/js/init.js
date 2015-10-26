5f352e7b4c4a25f0ba0181f1c9c3a276e8d7de0d
Fredrick Abayie <fredrick.abayie@gmail.com>
2015-10-26 13:23:14 +0000

    Makeover

--- www/js/init.js
+++ www/js/init.js
@@ -21,61 +21,143 @@ function sendRequest(u) {
 
 (function ($) {
     "use strict";
-    $(function () {
-        var url, obj, path;
-        obj = sendRequest(url);
-        if (obj.result === 1) {
+    $('#farmers_table').ready(function () {
+        var url, obj;
+        url ="";
+        if (obj.result === 1){
             var div = "";
-            for (var index in obj.) {
-                
+            for (var index in obj.farmers){
+                div += "<table><thead><tr>";
+                div += "<th data-field='farmer_name'>"+obj.farmers[index]+"</th>";
+                div += "<th data-field='farmer_number'>"+obj.farmers[index]+"</th>";
+                div += "<th data-field='farmer_name'>"+obj.farmers[index]+"</th>";
+                div += "<th data-field='farmer_name'>"+obj.farmers[index]+"</th>";
+                div += "</tr></thead><tbody><tr>";
+                div += "<td>"+obj.farmers[index].name+"</td>";
+                div += "<td>"+obj.farmers[index].phone+"</td>";
+                div += "<td>"+obj.farmers[index].produce_type+"</td>";
+                div += "<td>"+obj.farmers[index].location+"</td>";
+                div += "</tr><tbody></table>";
             }
-            $().html(div);
-        } else {
-            
+            $ ( "#farmers_table" ).html ( div );
         }
+        
     });
 }(jQuery));
 
-//            Function to load the list of task
-//            $ ( document ).ready ( function ( )
-//            {
-//               var url = "../controllers/user_controller.php?cmd=5";
-//               var obj = sendRequest ( url );
-//               var path = "";
-//                if ( obj.result === 1 )
-//                {
-//                    path = $(".path").val();
-//                    $("#profileimage").attr("src", path );
-//                    var div = "";
-//                    var timer;
-//                    for ( var index in obj.tasks )
-//                    {
-//                        div += "<div class='showcontentdetailsinnertile showcontentdetailsinnertile2'\n\
-//                                    onclick='getPreview ( "+obj.tasks [index].task_id+" )'>";
-//                        div += "<input class='showcontentdetailsinnertilecheckbox showcontentdetailsinnertilecheckbox2'\n\
-//                                    value="+obj.tasks [index].task_id+" name='todelete[]' type='checkbox'>";
-//                        div += "<div class='showcontentdetailsinnertilename'>";
-//                        div += "<span>"+obj.tasks [index].user_fname+" "+obj.tasks [index].user_sname+"</span>";
-//                        div += "<div class='showcontentdetailsinnertiledelete showcontentdetailsinnertiledelete2' \n\
-//                                    style='float:right; margin-right:10px'>";
-//                        div += "<a class='delete' style='padding: 7px' id="+obj.tasks [index].task_id+"><i id='deleteicon' \n\
-//                                    class='fa fa-trash-o'></i></a>";
-//                        div += "</div>";
-//                        div += "</div>";
-//                        div += "<div class='showcontentdetailsinnertiletitle'>\n\
-//                                    <span>"+obj.tasks [index].task_title+"</span></div>";
-//                        div += "<div class='showcontentdetailsinnertiledescription'>\n\
-//                                    <span>"+obj.tasks [index].task_description+"</span></div>";
-//                        div += "</div>";
-//                    }
-//                    $ ( ".showcontentdetailsinner" ).html ( div );
+
+
+
+(function ($) {
+    "use strict";
+    $('#farmers_table').ready(function () {
+        var url, obj;
+        url ="";
+        if (obj.result === 1){
+            var div = "";
+            for (var index in obj.price){
+                div += "<table><thead><tr>";
+                div += "<th data-field='farmer_name'>"+obj.price[index]+"</th>";
+                div += "<th data-field='farmer_number'>"+obj.price[index]+"</th>";
+                div += "<th data-field='farmer_name'>"+obj.price[index]+"</th>";
+                div += "</tr></thead><tbody><tr>";
+                div += "<td>"+obj.price[index].produce_type+"</td>";
+                div += "<td>"+obj.price[index].produce_quantity+"</td>";
+                div += "<td>"+obj.price[index].produce_price+"</td>";
+                div += "</tr><tbody></table>";
+            }
+            $ ( "#prices_table" ).html ( div );
+        }
+        
+    });
+}(jQuery));
+
+
+
+
+(function ($) {
+    "use strict";
+    $('#farmers_table').ready(function () {
+        var url, obj;
+        url ="";
+        if (obj.result === 1){
+            var div = "";
+            for (var index in obj.produce){
+                div += "<table><thead><tr>";
+                div += "<th data-field='farmer_name'>"+obj.produce[index]+"</th>";
+                div += "<th data-field='farmer_number'>"+obj.produce[index]+"</th>";
+                div += "<th data-field='farmer_name'>"+obj.produce[index]+"</th>";
+                div += "</tr></thead><tbody><tr>";
+                div += "<td>"+obj.produce[index].name+"</td>";
+                div += "<td>"+obj.produce[index].produce_quantity+"</td>";
+                div += "<td>"+obj.produce[index].produce_type+"</td>";
+                div += "</tr><tbody></table>";
+            }
+            $ ( "#produce_table" ).html ( div );
+        }
+        
+    });
+}(jQuery));
+
+
+////            Function to load the list of task
+//$ ( document ).ready ( function ( )
+//{
+//var url = "../controllers/user_controller.php?cmd=5";
+//var obj = sendRequest ( url );
+//var path = "";
+//if ( obj.result === 1 )
+//{
+//path = $(".path").val();
+//$("#profileimage").attr("src", path );
+//var div = "";
+//var timer;
+//for ( var index in obj.tasks )
+//{
+//div += "<div class='showcontentdetailsinnertile showcontentdetailsinnertile2'\n\
+//            onclick='getPreview ( "+obj.tasks [index].task_id+" )'>";
+//div += "<input class='showcontentdetailsinnertilecheckbox showcontentdetailsinnertilecheckbox2'\n\
+//            value="+obj.tasks [index].task_id+" name='todelete[]' type='checkbox'>";
+//div += "<div class='showcontentdetailsinnertilename'>";
+//div += "<span>"+obj.tasks [index].user_fname+" "+obj.tasks [index].user_sname+"</span>";
+//div += "<div class='showcontentdetailsinnertiledelete showcontentdetailsinnertiledelete2' \n\
+//            style='float:right; margin-right:10px'>";
+//div += "<a class='delete' style='padding: 7px' id="+obj.tasks [index].task_id+"><i id='deleteicon' \n\
+//            class='fa fa-trash-o'></i></a>";
+//div += "</div>";
+//div += "</div>";
+//div += "<div class='showcontentdetailsinnertiletitle'>\n\
+//            <span>"+obj.tasks [index].task_title+"</span></div>";
+//div += "<div class='showcontentdetailsinnertiledescription'>\n\
+//            <span>"+obj.tasks [index].task_description+"</span></div>";
+//div += "</div>";
+//}
+//$ ( ".showcontentdetailsinner" ).html ( div );
 ////                     $ ( "#divStatus" ).text ( "Found " + obj.products.length + " results" );
-//                }
-//                else
-//                {
+//}
+//else
+//{
 ////                        $ ( "#divStatus" ).text ( obj.message );
 ////                        $ ( "#divStatus" ).css ( "backgroundColor", "red" );
-//                }
+//}
 //
-//                timer = setTimeout ( '(this)', 1000 );
-//            });
+//timer = setTimeout ( '(this)', 1000 );
+//});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
